0678f43755c6c27e1782ecc7b5f27d4b4ee7a382
Jordan Castillo <jordanxcallaway@gmail.com>
2019-12-04 09:46:21 -0800

    cat carousel attempt

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+$('.thumbnail').on('click', event => {
+  const imgSrc = $(event.currentTarget).find('img').attr('src');
+  const imgAlt = $(event.currentTarget).find('img').attr('alt');
+  
+  $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+});
